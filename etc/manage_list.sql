8ea6a7bc678e70efe8bc4ab3f73a9f3f127780f5
ehddud135 <ehddud135@github.com>
2024-09-07 19:13:58 +0900

    Remove test file

--- etc/manage_list.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO "main"."Manager" ("name", "user_id", "created_at") VALUES ('황동영', 'U03A07Q6DNV', '2024-07-29');
-INSERT INTO "main"."Manager" ("name", "user_id", "created_at") VALUES ('박은재', 'U071ZPY392M', '2024-07-30');
-INSERT INTO "main"."Manager" ("name", "user_id", "created_at") VALUES ('홍진진', 'U03NVLJUQN4', '2024-07-30');
-INSERT INTO "main"."Manager" ("name", "user_id", "created_at") VALUES ('최정윤', 'U043YRJC4DD', '2024-07-30');
-INSERT INTO "main"."Manager" ("name", "user_id", "created_at") VALUES ('우종범', 'U04DPPB357Y', '2024-07-30');
-INSERT INTO "main"."Manager" ("name", "user_id", "created_at") VALUES ('김우성', 'U04629DH8D6', '2024-07-30');
--- seslack/customer/tests.py
+++
@@ -1,6 +0,0 @@
-import tabulate
-from django.test import TestCase
-from models import CustomerList
-
-# print(CustomerList.objects.all())
-# Create your tests here.
